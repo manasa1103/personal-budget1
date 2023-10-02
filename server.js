91c89e896d2358bfe533fcbc1e5a6f0ad3401594
Manasa Manchineni <mmanchin@uncc.com>
2023-10-02 06:54:19 +0530

    working code

--- server.js
+++ server.js
@@ -1,22 +1,24 @@
+
 const express = require('express');
+const cors = require('cors');
 const app = express();
-const fileSystem = require('fs');
-const importJSON = fileSystem.readFileSync('data.json', 'utf8');
 const port = 3000;
-const budgetData = JSON.parse(importJSON);
+const data = require("./data.json");
+
+app.use(cors());
 
 app.get('/hello', (req, res) => {
-res.send('Hello World!');
-});
-
-app.listen(port, () => {
-console.log(`Example app listening at http://localhost:${port}`);
-}
-);
-
-app.get('/budget', (req, res) => {
-    res.json(budgetData);
-});
-
-
-app.use('/',express.static('public'));
+    res.send('Hello World!');
+    });
+    
+    app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+    }
+    );
+    
+    app.get('/budget', (req, res) => {
+        res.json(data);
+    });
+    
+    
+    app.use('/',express.static('public'));
